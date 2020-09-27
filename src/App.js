c49b7bab74e2351b4e894718c1801edecd59c0df
Uday Pratap <udaypratap169@gmail.com>
2020-09-27 22:37:04 +0530

    using react hooks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
-class App extends Component{
-  state = {
-    persons :[
-      {name: "Uday" , age: 26},
-      {name: "Singh" , age: 25}
-    ]
-  }
+const App = props => {
+  
+  const [personsState,setPersonsState] = useState({
+      persons :[
+        {name: "Uday" , age: 23},
+        {name: "singh" , age: 26}
+      ],
+      otherState: 'some other state'
+    });
 
-  swithNameHandler = () => {
-    // console.log("clicked")
-    this.setState(
-      {
-        persons :[
-          {name: "Uday" , age: 23},
-          {name: "Pratap" , age: 25}
-        ]
-      }
-    )
-  }
-  render() {
+    const swithNameHandler = () => {
+      // console.log("clicked")
+      setPersonsState({
+          persons :[
+            {name: "Uday" , age: 23},
+            {name: "Pratap" , age: 25}
+          ]
+        });
+    };
+
     return (
       <div className="App">
         <h1>Hi, I am uday</h1>
         <p>This is working</p>
-        <button onClick={this.swithNameHandler}>Click Here</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I'm a Software Developer</Person>
+        <button onClick={swithNameHandler}>Click Here</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>I'm a Software Developer</Person>
       </div>
     );
-  }
 }
-
-
 export default App;
+
+
